ac70c3628d6bbe032b36700973e7772f55e0b364
liangheming <1062823840@qq.com>
2022-04-15 16:49:36 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.scss';
+import jpg1 from './assets/1.jpg';
+import jpg2 from './assets/2.jpg';
+import jpg3 from './assets/3.jpg';
 
+const container = document.getElementById('root')
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let srcs = [jpg1, jpg2, jpg3];
+let index = 0;
+let timer = null;
+
+function start () {
+  timer =setInterval(() => {
+    index = (index + 1) % 3;
+    render();
+  }, 1000);
+}
+
+function render () {
+  root.render(
+    <React.StrictMode>
+      <div className="container">
+        <img src={srcs[index]} alt=""/>
+      </div>
+    </React.StrictMode>
+  );
+}
+
+function stop () {
+  clearInterval(timer)
+}
+console.log(container);
+
+render();
+
+container.onmouseenter = () => {
+  stop();
+}
+container.onmouseleave = () => {
+  start();
+}
+
+
+
